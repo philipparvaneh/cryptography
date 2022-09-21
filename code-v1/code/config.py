b937efab54689fee032f1c002a854fc3a63cdd02
philipparvaneh <pp463@cornell.edu>
2022-09-21 15:57:16 -0400

    hw1

--- code-v1/code/config.py
+++
@@ -1,6 +0,0 @@
-BASE_URL = "https://bad.cryptography.fyi"
-AUTH_URL = BASE_URL + "/check_auth/"
-
-# TODO: put credentials here
-usr = ""
-key = ""
---
+++ hw0/code/config.py
@@ -0,0 +1,6 @@
+BASE_URL = "https://bad.cryptography.fyi"
+AUTH_URL = BASE_URL + "/check_auth/"
+
+# TODO: put credentials here
+usr = ""
+key = ""
