995928fe8adcc104803534c60e60947ee53180e7
philipparvaneh <pp463@cornell.edu>
2022-09-22 00:19:42 -0400

    q1-testing

--- hw1/hw1_recover_bias.py
+++ hw1/hw1_recover_bias.py
@@ -87,7 +87,9 @@ def recover_flag() -> bytes:
                         num[newCount[l][idx]] = 1
     byteNum = max(num, key=num.get)
     print(byteNum)
-
+    hexStr = hex(int(byteNum, 16) ^ int(hexNum, 16))
+    byteFlag[i] = int(hexStr, 16)
+    print(byteFlag)
     return flag
 
 
